894870a5790ad213ec5153580ef61ee7bc8d473c
lcmongwe <lcmongwe@gmail.com>
2022-03-21 18:19:49 +0300

    on click functions added to order buttons

---
+++ jquery.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $("#order").click(function () {
+    $("#order").hide();
+    $(".cont").show();
+  });
+  $("#place").click(function () {
+    $("#order").show();
+    $(".cont").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#order1").click(function () {
+    $("#order1").hide();
+    $(".cont").show();
+  });
+  $("#place").click(function () {
+    $("#order1").show();
+    $(".cont").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#order2").click(function () {
+    $("#order2").hide();
+    $(".cont").show();
+  });
+  $("#place").click(function () {
+    $("#order2").show();
+    $(".cont").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#order3").click(function () {
+    $("#order3").hide();
+    $(".cont").show();
+  });
+  $("#place").click(function () {
+    $("#order3").show();
+    $(".cont").hide();
+  });
+});
