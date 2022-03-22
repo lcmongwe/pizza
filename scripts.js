1ff45afa9851906f5140bbddda56d25ccf418dee
lcmongwe <lcmongwe@gmail.com>
2022-03-22 13:24:29 +0300

    cost array created

--- scripts.js
+++ scripts.js
@@ -13,42 +13,13 @@ function total() {
   var numberOfPizzas = document.getElementById("number").value;
   var location = document.getElementById("delivery").value;
 
-  akhanArray = [
-    ["Sunday", "Kwasi", "Akosua"],
-    ["Monday", "Kwado", "Adwoa"],
-    ["Tuesday", "Kwabena", "Abenaa"],
-    ["Wednesday", "Kwaku", "Akua"],
-    ["Thursday", "Yaw", "Yaa"],
-    ["Friday", "Kofi", "Afua"],
-    ["Saturday", "Kwame", "Ama"],
+  costArray = [
+    [1600, 1400, 1200],
+    [1450, 1250, 1050],
+    [1300, 1100, 900],
   ];
 
-  var dayOfTheWeek = akhanArray[day][0];
-  var maleName = akhanArray[day][1];
-  var femaleName = akhanArray[day][2];
   const output = document.getElementById("message");
-
-  if (gender == "") {
-    alert("please selet gender");
-  } else if (year > 2022) {
-    alert("you are not born yet");
-  } else if (gender == "male") {
-    output.innerHTML =
-      "Hey! your Akhan name is " +
-      maleName +
-      " " +
-      "you were born on" +
-      " " +
-      dayOfTheWeek;
-  } else if (gender == "female") {
-    output.innerHTML =
-      "Hey! your Akhan name is " +
-      femaleName +
-      " " +
-      "you were born on" +
-      " " +
-      dayOfTheWeek;
-  }
 
   formReset();
 }
